2866f320d316313af49770c37d6590be7f9cdeeb
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2016-03-11 17:48:06 +0100

    Check status not only against local holdings but against electronic holdings.

--- Classes/ViewHelpers/Find/HoldingStatusJsonViewHelper.php
+++ Classes/ViewHelpers/Find/HoldingStatusJsonViewHelper.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Slub\SlubFindExtend\ViewHelpers\Find;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Cache\CacheManager;
 
 /**
  * Class HoldingStatusJsonViewHelper
@@ -22,7 +24,7 @@ class HoldingStatusJsonViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstr
 	 * @param $exemplare
 	 * @return int
 	 */
-	private function getStatusFromArray($exemplare) {
+	private function getLocalHoldingStatusFromArray($exemplare) {
 
 		$status = 9999;
 
@@ -33,7 +35,7 @@ class HoldingStatusJsonViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstr
 			}
 
 			if($exemplar['elements'] && is_array($exemplar['elements'])) {
-				$status = $this->getStatusFromArray($exemplar['elements']);
+				$status = $this->getLocalHoldingStatusFromArray($exemplar['elements']);
 			} elseif ( ( $exemplar['_calc_colorcode'] < $status ) && ( $status != 1) ) {
 				if(!($exemplar['_calc_colorcode'] == 0 && ($status == 2))) {
 					$status = $exemplar['_calc_colorcode'];
@@ -42,6 +44,118 @@ class HoldingStatusJsonViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstr
 			}
 
 		}
+
+		return $status;
+
+	}
+
+	/**
+	 * Tries to resolve Article against holdings
+	 *
+	 */
+	private function getElectronicHoldingFromData($data) {
+
+		$status = [];
+
+		if(!$data['enriched']) return;
+
+		$article = $data['enriched']['fields']['rft.atitle'];
+		$firstISSN = $data['enriched']['fields']['rft.atitle'];
+		$volume = $data['enriched']['fields']['rft.volume'];
+		$spage = $data['enriched']['fields']['rft.spage'];
+		$epage = $data['enriched']['fields']['rft.epage'];
+		$pages = $data['enriched']['fields']['rft.pages'];
+		$issue = $data['enriched']['fields']['rft.issue'];
+		$genre = $data['enriched']['fields']['rft.genre'];
+		$date = $data['enriched']['fields']['rft.date'];
+		$language = $data['enriched']['fields']['languages'][0];
+		$doi = $data['enriched']['fields']['doi'];
+		$jtitle = $data['enriched']['fields']['rft.jtitle'];
+		$firstAuthor = (array)$data['enriched']['fields']['authors'][0];
+		$firstAuthorAulast = $firstAuthor['rft.aulast'];
+		$firstAuthorAufirst = $firstAuthor['rft.aufirst'];
+
+		$url = 'http://www-fr.redi-bw.de/links/?rl_site=slub&atitle='.urlencode($article).
+			'&issn='.urlencode($firstISSN).
+			'&volume='.urlencode($volume).
+			'&spage='.urlencode($spage).
+			'&epage='.urlencode($epage).
+			'&pages='.urlencode($pages).
+			'&issue='.urlencode($issue).
+			'&aulast='.urlencode($firstAuthorAulast).
+			'&aufirst='.urlencode($firstAuthorAufirst).
+			'&genre='.urlencode($genre).
+			'&sid=katalogbeta.slub-dresden.de&date='.urlencode($date).
+			'&language='.urlencode($language).
+			'&id='.urlencode($doi).
+			'&title='.urlencode($jtitle);
+
+		$doc = new \DOMDocument();
+		$doc->loadHTML(file_get_contents($url));
+
+		$xpath = new \DOMXpath($doc);
+
+		$infolink = $xpath->query("//span[contains(@class,'t_infolink')]/a/@href")->item(0)->nodeValue;
+
+		$access = $xpath->query("//div[@id ='t_ezb']/div/p/b")->item(0)->nodeValue;
+
+		$ezb_status = $xpath->query("//div[contains(@class,'t_ezb_result')]/p/span/@class")->item(0)->nodeValue;
+		$ezb_status_via = trim($xpath->query("//div[contains(@class,'t_ezb_result')]/p")->item(0)->nodeValue);
+		$ezb_url = $xpath->query("//div[contains(@class,'t_ezb_result')]/p/span[contains(@class,'t_link')]/a/@href")->item(0)->nodeValue;
+
+		$via = substr($ezb_status_via, strpos($ezb_status_via, 'via')+4, -4);
+
+		$status_code = 1;
+		switch ($ezb_status) {
+			case 't_ezb_red':
+				$status_code = 0;
+				break;
+		}
+
+		$status['infolink'] = $infolink;
+		$status['access'] = $access == 'freigeschaltet' ? 1 : 0;
+		$status['via'] = $via;
+		$status['url'] = $ezb_url;
+		$status['status'] = $status_code;
+
+		return $status;
+
+	}
+
+	/**
+	 * Tries to resolve Article against holdings
+	 *
+	 */
+	private function getElectronicDatabaseFromData($data) {
+
+		$databaseUrl = '';
+
+		foreach($data['documents'][0]['url'] as $url) {
+
+			if((strpos($url, 'http://www.bibliothek.uni-regensburg.de/dbinfo/frontdoor.php') === 0) ||
+				(strpos($url, 'http://rzblx10.uni-regensburg.de/dbinfo/detail.php?titel_id') === 0)) {
+				$databaseUrl = $url;
+			};
+		}
+
+
+		if(!strlen($databaseUrl)) return;
+
+
+		$doc = new \DOMDocument();
+		libxml_use_internal_errors(true);
+		$doc->loadHTML(file_get_contents($databaseUrl));
+
+		$xpath = new \DOMXpath($doc);
+
+		$dbis_url = $xpath->query("//td[@id ='detail_content_start']/a/@href")->item(0)->nodeValue;
+
+
+		$status['infolink'] = $databaseUrl;
+		$status['access'] = 1;
+		$status['via'] = '';
+		$status['url'] = 'http://rzblx10.uni-regensburg.de/dbinfo/'.$dbis_url;
+		$status['status'] = 1;
 
 		return $status;
 
@@ -58,19 +172,50 @@ class HoldingStatusJsonViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstr
 
 			if($data['enriched']['fields']['exemplare']) {
 
-				$status = $this->getStatusFromArray($data['enriched']['fields']['exemplare']);
+				$status = $this->getLocalHoldingStatusFromArray($data['enriched']['fields']['exemplare']);
+				return json_encode(array('status' => $status));
 
 			} else {
-
 				// Somehow this is a Local Holdings file with no copies. Send "Action needed" state.
 				return json_encode(array('status' => 0));
 			}
 
-		} elseif($data['documents'][0]['access_facet'] =="Electronic Resources") {
-			$status = 1;
-		}
+		} elseif(($data['documents'][0]['access_facet'] =="Electronic Resources") || ($data['documents'][0]['physical'] && in_array('Online-Ressource', $data['documents'][0]['physical']))) {
 
-		return json_encode(array('status' => $status));
+			$cache = GeneralUtility::makeInstance(CacheManager::class)->getCache('resolv_link_electronic');
+			$cacheIdentifier = sha1($data['documents'][0]['id']);
+			$entry = $cache->get($cacheIdentifier);
+			if (!$entry) {
+
+				// Try to resolve article against holdings
+				$entry = $this->getElectronicHoldingFromData($data);
+
+				// Try to resolve article against databases
+				if(strlen($entry['url']) === 0) {
+					$entry = $this->getElectronicDatabaseFromData($data);
+				}
+
+				// Still no luck? Fall back to first url
+
+				if(strlen($entry['url']) === 0) {
+					$entry = [
+						'infolink' => '',
+						'access' => 1,
+						'via' => 1,
+						'url' => $data['documents'][0]['url'][0],
+						'status' => 1
+					];
+				}
+
+				//$cache->set($cacheIdentifier, $entry);
+			}
+
+			$entry['url'] = 'http://wwwdb.dbod.de/login?url='.$entry['url'];
+
+			return json_encode($entry);
+		} else {
+			return json_encode(array('status' => 0));
+		}
 
 	}
 
--- ext_localconf.php
+++ ext_localconf.php
@@ -57,3 +57,11 @@ $signalSlotDispatcher->connect(
     'blacklist',
     FALSE
 );
+
+$cacheKey = 'resolv_link_electronic';
+if( !is_array( $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ] ) ){
+    $cacheConfig =  $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ] = array();
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ]['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ]['backend'] = 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend';
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ]['options'] = array();
+}
