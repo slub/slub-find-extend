fb23a67225c208df4898f4c5753bdf0cee37c167
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2016-03-30 22:05:21 +0200

    Add limit to RemoveParenthesesTextViewHelper

--- Classes/ViewHelpers/Format/RemoveParenthesesTextViewHelper.php
+++ Classes/ViewHelpers/Format/RemoveParenthesesTextViewHelper.php
@@ -9,17 +9,30 @@ namespace Slub\SlubFindExtend\ViewHelpers\Format;
 class RemoveParenthesesTextViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
     /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments() {
+        parent::initializeArguments();
+        $this->registerArgument('content', 'string', 'Content string', FALSE);
+        $this->registerArgument('limit', 'int', 'Limit replacments', FALSE, -1);
+    }
+
+
+    /**
      * Removes any text inside a parentheses including the parentheses
      *
-     * @param string $content Content string
      * @return string
      */
-    public function render($content = NULL) {
+    public function render() {
+
+        $content = $this->arguments['content'];
+
         if ($content === NULL) {
             $content = $this->renderChildren();
         }
 
-        return preg_replace("/\([^)]+\)/","",$content);
+        return preg_replace("/\([^)]+\)/","",$content, $this->arguments['limit']);
     }
 
 }
