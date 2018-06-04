d9191f90f6f4f4e21f0589066e47876ee0fcca68
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2018-06-04 22:24:48 +0200

    Erhöhung Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.0.1',
+	'version' => '0.1.0',
     'autoload' =>
         array(
             'psr-4' =>
