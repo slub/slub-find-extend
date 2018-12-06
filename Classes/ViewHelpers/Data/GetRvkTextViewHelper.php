e81ba150b3317e007d7f9af3407c40f6b18562ff
Norman Steger <34568672+ByteParty@users.noreply.github.com>
2018-12-06 10:20:52 +0100

    rewritten rvk viewhelper (#12)

--- Classes/ViewHelpers/Data/GetRvkTextViewHelper.php
+++ Classes/ViewHelpers/Data/GetRvkTextViewHelper.php
@@ -1,66 +1,54 @@
 <?php
 namespace Slub\SlubFindExtend\ViewHelpers\Data;
 
-	/***************************************************************
-	 *
-	 *  Copyright notice
-	 *
-	 *  This script is part of the TYPO3 project. The TYPO3 project is
-	 *  free software; you can redistribute it and/or modify
-	 *  it under the terms of the GNU General Public License as published by
-	 *  the Free Software Foundation; either version 3 of the License, or
-	 *  (at your option) any later version.
-	 *
-	 *  The GNU General Public License can be found at
-	 *  http://www.gnu.org/copyleft/gpl.html.
-	 *
-	 *  This script is distributed in the hope that it will be useful,
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *  GNU General Public License for more details.
-	 *
-	 *  This copyright notice MUST APPEAR in all copies of the script!
-	 ***************************************************************/
+    /***************************************************************
+     *
+     *  Copyright notice
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 3 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     ***************************************************************/
 
 /**
  * GetRvkTextViewHelper
  */
 class GetRvkTextViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
-	/**
-	 * Replaces RVK with result from RVK API
-	 *
-	 * @param string $rvk RVK
-	 * @return string
-	 */
-	public function render($rvk = NULL) {
+    /**
+     * Replaces RVK with result from RVK API
+     *
+     * @param string $rvk RVK
+     * @return string
+     */
+    public function render($rvk = NULL) {
 
-		$rvk_string = '';
 
-		if ($rvk === NULL) {
-			$rvk = $this->renderChildren();
-		}
+        if ($rvk === NULL) {
+            $rvk = $this->renderChildren();
+        }
 
-		$rvk_obj = json_decode(@file_get_contents('http://rvk.uni-regensburg.de/api/json/children/'.urlencode(trim($rvk))));
+        $url = 'http://sdvkatalogrvk.slub-dresden.de/api/?rvk='.urlencode(trim($rvk));
 
-		if($rvk_obj === FALSE) {
-			return $rvk;
-		}
-		else {
-			if ($rvk_obj->{'error-code'} > 0) {
-				return $rvk;
-			} else {
+        $rvkArray = json_decode(@file_get_contents($url),true);
 
-				$node = (array)$rvk_obj->{'node'};
+        if ( !empty( $rvkArray["name"] ) ) {
+            return trim($rvk) . ' : ' . $rvkArray["name"];
+        }
 
-				if ($node["benennung"] && strlen($node["benennung"]) > 0) {
-
-					return trim($rvk) . ' : ' . utf8_encode($node["benennung"]);
-				} else {
-					return $rvk;
-				}
-			}
-		}
-	}
+        return $rvk;
+    }
 
 }
