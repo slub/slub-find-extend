64e4aa10b7d77f1e8b620a75d0a4aede4bc3e6ac
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2019-03-16 23:18:35 +0100

    Ergebnisse mit einem Trefer anders behandeln

---
+++ Classes/Slots/HandleOneHit.php
@@ -0,0 +1,82 @@
+<?php
+namespace Slub\SlubFindExtend\Slots;
+
+use Solarium\QueryType\Select\Result\Document;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+
+/**
+ * Slot implementation before the
+ *
+ * @category    Slots
+ * @package     TYPO3
+ */
+class HandleOneHit {
+
+    const IDFIELDS = ['record_id', 'barcode', 'rsn', 'isbn', 'ismn', 'issn', 'zdb'];
+
+    /**
+     * Contains the settings of the current extension
+     *
+     * @var array
+     * @api
+     */
+    protected $settings;
+
+    /**
+     * @var UriBuilder
+     */
+    protected $uriBuilder;
+
+    /**
+     * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface
+     */
+    protected $configurationManager;
+
+    /**
+     * @param \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager
+     * @return void
+     */
+    public function injectConfigurationManager(ConfigurationManagerInterface $configurationManager) {
+        $this->configurationManager = $configurationManager;
+        $this->settings = $this->configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS);
+    }
+
+    public function injectUriBuilder(UriBuilder $uriBuilder) {
+        $this->uriBuilder = $uriBuilder;
+    }
+    /**
+     * Slot to enrich finds detail view
+     *
+     * @param array &$resultSet
+     */
+    public function index(&$resultSet)
+    {
+        $idhit = false;
+
+        if ($resultSet->getNumFound() === 1) {
+
+            /* @var $document Document */
+            $document = $resultSet->getDocuments()[0];
+
+            foreach ($resultSet->getHighlighting()->getResult($document['id'])->getFields() as $key => $value) {
+                if(in_array($key, $this::IDFIELDS)) {
+                    $idhit = true;
+                }
+            }
+
+            if($idhit) {
+
+                $uri = $this->uriBuilder->uriFor("detail", ['id' => $document['id'], 'underlyingQuery' => ['q' => $_GET['tx_find_find']['q'], 'position' => 1]], "Search", "find", "Find");
+
+                header("Location: " . $uri, TRUE, 302);
+                die();
+            }
+
+        }
+
+    }
+
+
+
+}
