2b875b00755b629cbe24e1e25388ac59dadaaa98
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2021-01-23 17:00:36 +0100

    RenderUser TYPO3 9.5 compatibility

--- Classes/ViewHelpers/Data/RenderUserViewHelper.php
+++ Classes/ViewHelpers/Data/RenderUserViewHelper.php
@@ -9,6 +9,13 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 class RenderUserViewHelper extends AbstractViewHelper {
 
+    /**
+     * As this ViewHelper renders HTML, the output must not be escaped.
+     *
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
     public function render (){
 
         $this->templateVariableContainer->add('user', $GLOBALS['TSFE']->fe_user->user);
