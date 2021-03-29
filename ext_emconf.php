9b8469d4c4347e29c835531f4929eb70c886a1f7
Norman Steger <norman.steger@slub-dresden.de>
2021-03-29 15:46:14 +0200

    rise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.1.0',
+	'version' => '0.2.0',
     'autoload' =>
         array(
             'psr-4' =>
@@ -31,8 +31,8 @@ $EM_CONF[$_EXTKEY] = array(
         ),
 	'constraints' => array(
 		'depends' => array(
-            'php' => '5.5.0-7.3.99',
-            'typo3' => '6.2.0-9.5.99'
+            'php' => '7.2.0-7.3.99',
+            'typo3' => '9.5.0-9.5.99'
 		),
 		'conflicts' => array(
 		),
