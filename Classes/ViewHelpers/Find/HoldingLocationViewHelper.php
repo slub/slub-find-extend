6af898c6dd2c43cbb1c2694be00077d1aa83ddb6
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2021-10-25 18:05:36 +0200

    ViewHelper to show first holding location

---
+++ Classes/ViewHelpers/Find/HoldingLocationViewHelper.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Slub\SlubFindExtend\ViewHelpers\Find;
+
+/**
+ *
+ */
+
+use Slub\SlubFindExtend\Services\HoldingStatusService;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+
+class HoldingLocationViewHelper extends AbstractViewHelper
+{
+
+	/**
+	 * @var \Slub\SlubFindExtend\Services\HoldingStatusService
+	 */
+	protected static $holdingStatusService;
+
+    public function initializeArguments() {
+        parent::initializeArguments();
+        $this->registerArgument('document', 'object', 'The index document', TRUE);
+        $this->registerArgument('copies', 'array', 'The the holded copies', FALSE, []);
+    }
+
+    /**
+     * @return string
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+
+		if ($arguments['document']) {
+
+		    $document = $arguments['document'];
+
+			$status = static::getHoldingStatusService()->getStatus($document, $arguments['copies']);
+
+            if(($status === 1) && ($document['access_facet'] === 'Local Holdings')) {
+                foreach ($arguments['copies'] as $exemplar) {
+                    if ($exemplar['_calc_colorcode'] == 1) {
+                        return $exemplar['Regalstandort'];
+                    }
+                }
+            }
+            return '';
+		} else {
+			return '';
+		}
+	}
+
+    private static function getHoldingStatusService()
+    {
+        if (null === static::$holdingStatusService) {
+            $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+            static::$holdingStatusService = $objectManager->get(HoldingStatusService::class);
+        }
+
+        return static::$holdingStatusService;
+    }
+
+
+}
