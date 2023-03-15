944495b2aa0df7079ca9921feac27d922ad09f64
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2023-03-15 18:05:43 +0100

    [Feature] Solr enrichment, fix Tab error, save log, send task

---
+++ Classes/Task/SendEnrichSolrResultLogTask.php
@@ -0,0 +1,98 @@
+<?php
+namespace Slub\SlubFindExtend\Task;
+
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2023 SLUB Dresden
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+use Symfony\Component\Mime\Address;
+use TYPO3\CMS\Core\Mail\MailMessage;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class SendEnrichSolrResultLogTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
+
+    /**
+     * E-Mails to send log to
+     *
+     * @var string
+     */
+    protected $emails;
+
+
+    public function execute() {
+
+        $successfullyExecuted = true;
+
+        if(file_exists(\TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/EnrichSolrResult.log')) {
+
+            rename(\TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/EnrichSolrResult.log', \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/EnrichSolrResult_process.log');
+
+            $mail = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Mail\MailMessage::class);
+            $mail
+                ->setSubject('SLUB Katalog: Enrichment Fehler vom '.date("d.m.Y"))
+                ->setFrom(array('typo3@slub-dresden.de' => 'SLUB TYPO3 Server'))
+                ->setTo($this->getEmails())
+                ->setBody(file_get_contents(\TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/EnrichSolrResult_process.log'))
+                ->send();
+
+            unlink(\TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/EnrichSolrResult_process.log');
+
+        } else {
+
+            $mail = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Mail\MailMessage::class);
+            $mail
+                ->setSubject('SLUB Katalog: Enrichment Fehler vom '.date("d.m.Y"))
+                ->setFrom(array('typo3@slub-dresden.de' => 'SLUB TYPO3 Server'))
+                ->setTo($this->getEmails())
+                ->setBody('Heute keine Fehler .... :)')
+                ->send();
+
+        }
+
+        return $successfullyExecuted;
+            
+    }
+
+    /**
+     * Set the value of the emails
+     *
+     * @param string $emails E-Mails to send log to
+     *
+     * @return void
+     */
+    public function setEmails($emails)
+    {
+        $this->emails = $emails;
+    }
+
+    /**
+     * Get the value of the emails
+     *
+     * @return string $emails E-Mails to send log to
+     */
+    public function getEmails()
+    {
+        return $this->emails;
+    }
+}
---
+++ Classes/Task/SendEnrichSolrResultLogTaskAdditionalFieldProvider.php
@@ -0,0 +1,114 @@
+<?php
+namespace Slub\SlubFindExtend\Task;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017 Alexander Bigga <alexander.bigga@slub-dresden.de>, SLUB Dresden
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package slub_find_extend
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class SendEnrichSolrResultLogTaskAdditionalFieldProvider implements \TYPO3\CMS\Scheduler\AdditionalFieldProviderInterface
+{
+
+    /**
+     * Render additional information fields within the scheduler backend.
+     *
+     * @param array                                                     $taskInfo        Array information of task to return
+     * @param CleanUpTask                                            $task            Task object
+     * @param \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule Reference to the BE module of the Scheduler
+     *
+     * @return array Additional fields
+     * @see \TYPO3\CMS\Scheduler\AdditionalFieldProviderInterface->getAdditionalFields($taskInfo, $task, $schedulerModule)
+     */
+    public function getAdditionalFields(
+        array &$taskInfo,
+        $task,
+        \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule
+    ) {
+        $additionalFields = [];
+
+        if (empty($taskInfo['emails'])) {
+            if ($schedulerModule->CMD == 'add') {
+                $taskInfo['emails'] = '';
+            } elseif ($schedulerModule->CMD == 'edit') {
+                $taskInfo['emails'] = $task->getEmails();
+            } else {
+                $taskInfo['emails'] = $task->setEmails();
+            }
+        }
+
+        $fieldId = 'task_emails';
+        $fieldCode = '<input type="text" class="form-control" name="tx_scheduler[slub_find_extend][emails]" id="' . $fieldId . '" value="' . htmlspecialchars($taskInfo['emails']) . '"/>';
+        $label = $GLOBALS['LANG']->sL('LLL:EXT:slub_find_extend/Resources/Private/Language/locallang_be.xlf:tasks.enricherrorlog.emails');
+        $additionalFields[$fieldId] = [
+            'code'  => $fieldCode,
+            'label' => $label
+        ];
+
+        return $additionalFields;
+    }
+
+    /**
+     * This method checks any additional data that is relevant to the specific task.
+     * If the task class is not relevant, the method is expected to return TRUE.
+     *
+     * @param array                                                     $submittedData   Reference to the array containing the data submitted by the user
+     * @param \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule Reference to the BE module of the Scheduler
+     *
+     * @return boolean TRUE if validation was ok (or selected class is not relevant), FALSE otherwise
+     */
+    public function validateAdditionalFields(
+        array &$submittedData,
+        \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule
+    ) {
+
+        if(strlen($submittedData['slub_find_extend']['emails']) > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * This method is used to save any additional input into the current task object
+     * if the task class matches.
+     *
+     * @param array                                  $submittedData Array containing the data submitted by the user
+     * @param \TYPO3\CMS\Scheduler\Task\AbstractTask $task          Reference to the current task object
+     *
+     * @return void
+     */
+    public function saveAdditionalFields(array $submittedData, \TYPO3\CMS\Scheduler\Task\AbstractTask $task)
+    {
+        /** @var $task CleanUpTask */
+        $task->setEmails($submittedData['slub_find_extend']['emails']);
+    }
+}
