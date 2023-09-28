4b9d5ca1288f915b067c817d9202906f52e03f78
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2023-09-28 17:07:34 +0200

    Add more Hosts not getting Proxy prefix

--- Classes/Services/LinksFromMarcFullrecordService.php
+++ Classes/Services/LinksFromMarcFullrecordService.php
@@ -29,7 +29,7 @@ class LinksFromMarcFullrecordService
     public function getLinks($fullrecord, $isil = null, $unique = false, $merged = false)
     {
         $defaultPrefix = 'http://wwwdb.dbod.de/login?url=';
-        $noPrefixHosts = ['wwwdb.dbod.de', 'www.dbod.de', 'nbn-resolving.de', 'digital.slub-dresden.de', 'digital.zlb.de', 'www.deutschefotothek.de'];
+        $noPrefixHosts = ['wwwdb.dbod.de', 'www.dbod.de', 'nbn-resolving.de', 'digital.slub-dresden.de', 'digital.zlb.de', 'www.deutschefotothek.de', 'mediathek.slub-dresden.de', 'ezb.ur.de', 'dbis.uni-regensburg.de', 'www.bibliothek.uni-regensburg.de',];
         $blacklistLabel = ['Kostenfrei', 'Volltext'];
 
         $resourceLinks = [];
