64cf8fbd1aff0a6a95f86ef0310f91d3347e9eea
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2024-11-28 13:42:28 +0100

    Add ArrayPluckViewHelper

---
+++ Classes/ViewHelpers/Data/ArrayPluckViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Slub\SlubFindExtend\ViewHelpers\Data;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class ArrayPluckViewHelper extends AbstractViewHelper
+{
+    /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('array', 'array', 'The array to pluck values from', true);
+        $this->registerArgument('pluck', 'string|array', 'String or array to remove', true);
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+
+     * @return string
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $array = $arguments['array'];
+        $pluck = $arguments['pluck'];
+
+        if (!is_array($pluck)) {
+            $pluck = [$pluck];
+        }
+
+        $array = array_diff($array, $pluck);
+
+        return $array;
+    }
+}
